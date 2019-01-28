41320b8831e8cd285e5704266366d171a9365084
ecollazodominguez <ecollazodominguez@nautilus9.danielcastelao.org>
2019-01-28 10:58:32 +0100

    ejercicio4

---
+++ Boletin19_4/src/boletin19_4/Boletin19_4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin19_4;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Boletin19_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dni persona = new Dni(53194047);
+        persona.calcularLetra();
+    }
+    
+}
---
+++ Boletin19_4/src/boletin19_4/Dni.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin19_4;
+
+/**
+ *
+ * @author ecollazodominguez
+ */
+public class Dni {
+    private int id;
+    private final String []letra ={"T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E"};
+
+    public Dni(int id) {
+        this.id = id;
+    }
+
+    public Dni() {
+    }
+    
+    public int resto(int id){
+        this.id = id;
+        int resto;
+        resto = id%23;
+        return resto;
+    }
+    
+    public void calcularLetra(){
+        System.out.println("A letra deste dni é: "+letra[this.resto(id)]);
+        }
+    }
